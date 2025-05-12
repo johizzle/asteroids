c406305b1d982cd9218b853562390791699c420e
johizzle <e.johanne.eriksson@gmail.com>
2025-05-12 10:30:02 +0200

    Added FPS

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -18,6 +20,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+
 
 
 
