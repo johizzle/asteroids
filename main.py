7734c949226323c498dcc2198c30d567ffeeb46b
johizzle <e.johanne.eriksson@gmail.com>
2025-05-13 14:07:14 +0200

    Added player class and initiated !

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 from constants import *
 from circleshape import *
+from player import *
 
 def main():
     pygame.init()
@@ -12,7 +13,10 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         #this will check if the user has closed the window and exit 
         #the game loop if they do
@@ -20,6 +24,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
