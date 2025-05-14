91b205601b7a95b5901d3765f110d83e062db4ec
johizzle <e.johanne.eriksson@gmail.com>
2025-05-14 10:06:18 +0200

    refactored update and draw

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -24,10 +27,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
+
         
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for drawable_obj in drawable:
+            drawable_obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
